8ac48af5588ab8daebb82c488238690cd9c97b8e
John-A-Aydin <johnaaydin@gmail.com>
2024-09-24 10:06:33 -0400

    Random update that needs to be overwritten

--- src/JohnA1MancalaNode.java
+++ src/JohnA1MancalaNode.java
@@ -4,36 +4,45 @@
  */
 public class JohnA1MancalaNode extends MancalaNode {
 
-	/**
-	 * See corresponding <code>MancalaNode</code> standard constructor documentation.
-	 */
-	public JohnA1MancalaNode() {
-		super();
-	}
+  /**
+   * See corresponding <code>MancalaNode</code> standard constructor documentation.
+   */
+  public JohnA1MancalaNode() {
+    super();
+  }
 
-	/**
-	 * See corresponding <code>MancalaNode</code> copy constructor documentation.
-	 * @param node node to be copied
-	 */
-	public JohnA1MancalaNode(MancalaNode node) {
-		super(node);
-	}
+  /**
+   * See corresponding <code>MancalaNode</code> copy constructor documentation.
+   * @param node node to be copied
+   */
+  public JohnA1MancalaNode(MancalaNode node) {
+    super(node);
+  }
 
-	/**
-	 * See corresponding <code>MancalaNode</code> FairKalah constructor documentation.
-	 * @param stateIndex FairKalah initial state index
-	 */
-	public JohnA1MancalaNode(int stateIndex) {
-		super(stateIndex);
-	}
+  /**
+   * See corresponding <code>MancalaNode</code> FairKalah constructor documentation.
+   * @param stateIndex FairKalah initial state index
+   */
+  public JohnA1MancalaNode(int stateIndex) {
+    super(stateIndex);
+  }
 
-	/**
-	 * Return the difference between current MAX and MIN score.
-	 */
-	public double utility() {
-		double scoreDiff =  state[MAX_SCORE_PIT] - state[MIN_SCORE_PIT];
-
-		return scoreDiff;
-	}
-
+  /**
+   * Return the difference between current MAX and MIN score.
+   */
+  public double utility() {
+    double scoreDiff = state[MAX_SCORE_PIT] - state[MIN_SCORE_PIT];
+    // Try looking at possible range of score gain for next turn
+    // What function will produce the best results
+    //
+    // My play pits: 0-5
+    // My goal: 6
+    // Other play pits: 7-12
+    // Other goal: 13
+    for (int i = 0; i < state.length; i++) {
+      if (i + state[i] <= 5) { // This move will allow a pit steal
+      }
+    }
+    return scoreDiff;
+  }
 }
